3644771d0e9eed0614f02ed184acfe2aeace8c44
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-10-09 23:02:35 +0530

    Add files via upload

---
+++ userinput.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	interest := "like cricket alot"
+	fmt.Println(interest)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Rate your cricket knowlegde")
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for your reveiw", input)
+	fmt.Printf("Type of input is %T ", input)
+
+}
